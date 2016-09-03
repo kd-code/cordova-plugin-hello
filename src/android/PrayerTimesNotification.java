223bc75efb9c171a40e0f52a758721baf068ebb8
Mahdi Daneshvar <mahdidaneshvar.aka.haskell@gmail.com>
2016-09-03 18:02:11 +0430

    ADDED: some code and cooment

--- src/android/PrayerTimesNotification.java
+++ src/android/PrayerTimesNotification.java
@@ -1,10 +1,10 @@
 package com.smgroup;
 
 import org.apache.cordova.*;
-//import org.apache.cordova.api.CallbackContext;
-//import org.apache.cordova.api.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.widget.Toast;
 
 public class PrayerTimesNotification extends CordovaPlugin {
@@ -15,8 +15,12 @@ public class PrayerTimesNotification extends CordovaPlugin {
         if (action.equals("init"))
         {
             
-        	Toast toast = Toast.makeText(this.cordova.getActivity().getApplicationContext(),data.getString(0)+"here is the data", Toast.LENGTH_LONG);
+        	JSONObject reader = new JSONObject(data.getString(0));
+        	
+        	Toast toast = Toast.makeText(this.cordova.getActivity().getApplicationContext(),reader.getString("text"), Toast.LENGTH_LONG);
             toast.show();
+            //save data in shared prefs
+            init();
             return true;
         }
         else
@@ -24,4 +28,18 @@ public class PrayerTimesNotification extends CordovaPlugin {
             return false;
         }
     }
+    private void init()
+    {
+    	//clear previous notifications
+    	//read data from shared prefs
+    	//set notification for the next 48 hours
+    }
+    /*todo
+     * parse json and fill the data in the shared prefs
+     * write boot alarm receiver
+     * call init on boot
+     * write azan time alarm receiver
+     */
+    
 }
+
