907dadbbcdc1276f87733572ba4b4d66f25d7ec5
Mahdi Daneshvar <mahdidaneshvar.aka.haskell@gmail.com>
2016-09-08 16:39:59 +0430

    ADDED: PrayTime class and code to test it
    Also added input parsing

--- src/android/PrayerTimesNotification.java
+++ src/android/PrayerTimesNotification.java
@@ -1,5 +1,8 @@
 package com.smgroup;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,13 +28,7 @@ public class PrayerTimesNotification extends CordovaPlugin {
         	SharedPreferences prefs = this.cordova.getActivity().getSharedPreferences("azanplugin", Context.MODE_PRIVATE);
         	prefs.edit().putString("input_json", data.getString(0));
         	prefs.edit().commit();
-        	
         	init(data.getString(0),this.cordova.getActivity().getApplicationContext());
-        	
-        	/*JSONObject reader = new JSONObject(data.getString(0));
-        	
-        	Toast toast = Toast.makeText(this.cordova.getActivity().getApplicationContext(),reader.getString("text"), Toast.LENGTH_LONG);
-            toast.show();*/
             return true;
         }
         else
@@ -42,16 +39,58 @@ public class PrayerTimesNotification extends CordovaPlugin {
     public static void init(String input,Context ctx) throws JSONException
     {
     	//clear previous notifications
-    	
+    
     	
     	//read data from shared prefs
-		//SharedPreferences prefs = ctx.getSharedPreferences("azanplugin", Context.MODE_PRIVATE);
-		//input = prefs.getString("input_json", "");//second arg is the default value
-		//Log.d(TAG, "input is [" +  input+"]");
-		JSONObject reader = new JSONObject(input);
+    	JSONObject reader = new JSONObject(input);
+    	double lat = reader.getDouble("lat");
+    	double lng = reader.getDouble("lng");
+    	double timezone = reader.getDouble("timezone");
+    	String method = reader.getString("method");
+    	
+    	PrayTime prayers= new PrayTime();
+    	prayers.setTimeFormat(prayers.Time24);
+    	prayers.setCalcMethod(calcMethodStringToInt(method));
+    	prayers.setAsrJuristic(prayers.Shafii);
+    	prayers.setAdjustHighLats(prayers.AngleBased);
+    	
+    	Date now = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(now);
+        
+    	
+        ArrayList<String> prayerTimes = prayers.getPrayerTimes(cal,
+                lat, lng, timezone);
+        ArrayList<String> prayerNames = prayers.getTimeNames();
+    	
+        for (int i = 0; i < prayerTimes.size(); i++)
+        {
+            Log.d(TAG,prayerNames.get(i) + " - " + prayerTimes.get(i));
+        }
+		
     	Toast toast = Toast.makeText(ctx,reader.getString("text"), Toast.LENGTH_LONG);
         toast.show();
     	//set notification for the next 48 hours
+    }
+    private static int calcMethodStringToInt(String calc_method)
+    {
+    	calc_method = calc_method.toLowerCase();
+    	PrayTime pr = new PrayTime();
+    	if(calc_method=="jafari")
+    		return pr.Jafari;
+    	if(calc_method=="karachi")
+    		return pr.Karachi;
+    	if(calc_method=="isna")
+    		return pr.ISNA;
+    	if(calc_method=="mwl")
+    		return pr.MWL;
+    	if(calc_method=="makkah")
+    		return pr.Makkah;
+    	if(calc_method=="egypt")
+    		return pr.Egypt;
+    	if(calc_method=="tehran")
+    		return pr.Tehran;
+    	return pr.Tehran;
     }
     /*todo
      * parse json and fill the data in the shared prefs
@@ -59,6 +98,5 @@ public class PrayerTimesNotification extends CordovaPlugin {
      * call init on boot
      * write azan time alarm receiver
      */
-    
 }
 
