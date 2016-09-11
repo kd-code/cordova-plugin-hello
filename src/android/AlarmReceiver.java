48b19872caf0852b792ee9d0a68b894d473b1c53
kdcode <mahdidaneshvar@gmail.com>
2016-09-12 03:42:44 +0430

    ADDED: code to play mp3 fle from www folder

--- src/android/AlarmReceiver.java
+++ src/android/AlarmReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -19,6 +21,10 @@ public class AlarmReceiver extends BroadcastReceiver
 		Log.d(PrayerTimesNotification.TAG,"recieved");
     	Log.d(PrayerTimesNotification.TAG,data);
     	Log.d(PrayerTimesNotification.TAG,"time is: " + intent.getExtras().getString("time_name"));
+    	
+    	MediaPlayer mp = MediaPlayer.create(context, Uri.parse("file://android_asset/www/sound.mp3"));
+    	mp.start();
+    	
     	
 	}  
 }
