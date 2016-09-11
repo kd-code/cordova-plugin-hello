19bb9ff1398dbe754801240038539f8c8eb7880b
kdcode <mahdidaneshvar@gmail.com>
2016-09-12 03:51:42 +0430

    FIXED: media player code

--- src/android/AlarmReceiver.java
+++ src/android/AlarmReceiver.java
@@ -1,12 +1,16 @@
 package com.smgroup;
 
+import java.io.IOException;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -22,9 +26,20 @@ public class AlarmReceiver extends BroadcastReceiver
     	Log.d(PrayerTimesNotification.TAG,data);
     	Log.d(PrayerTimesNotification.TAG,"time is: " + intent.getExtras().getString("time_name"));
     	
-    	MediaPlayer mp = MediaPlayer.create(context, Uri.parse("file://android_asset/www/sound.mp3"));
-    	mp.start();
-    	
-    	
+    	AssetFileDescriptor desc;
+		try {
+			desc = context.getApplicationContext().getAssets().openFd("www/sound.mp3");
+			MediaPlayer mp = new MediaPlayer();
+	    	mp.setDataSource(desc.getFileDescriptor(),desc.getStartOffset(),desc.getLength());
+	    	mp.prepare();
+	    	mp.setLooping(false);
+	    	mp.start();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	//NotificationCompat.Builder b = new NotificationCompat.Builder(context);
 	}  
 }
